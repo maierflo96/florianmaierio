4a51955b8c582fbf1b8fdac3ebd28c6a49a72ba1
Florian Maier <maier.3010@gmail.com>
2021-01-14 09:40:26 +0100

    changed design

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -5,25 +5,30 @@
 
       <md-app-toolbar style="background-color:grey">
 
+
         <span class="md-title" style="color: aliceblue" >
           <a style="color: aquamarine">f</a>lorian<a style="color: aquamarine">m</a>aier.<a style="color: orangered">io</a><a class="blinking">|</a></span>
+
+
+        <img src="../assets/logo.png" class="logo"/>
+
       </md-app-toolbar>
     <md-app-content>
       <div style="padding-top:20px">
       <h1>Design.</h1>
-      <h1>Code.</h1>
+      <h1 style="color:orangered">Code.</h1>
       <h1>Passion.</h1>
     </div>
     <div>
       <md-divider></md-divider>
-        <div class="md-layout md-gutter md-alignment-center">
+        <div class="md-layout md-gutter md-alignment-center" style="padding-top: 30px">
           <div class="md-layout-item md-medium-size-75 md-small-size-50 md-xsmall-size-100" style="padding-top:40px;margin-left:40px">
-          <h2> I'm a software developer based in Vienna, Austria. I have  been working as Java and JavaScript developer at a few major companies. For work I have been programming Java Spring Applications, as well as both Front- and Backend NodeJs Applications.
+          <h2> I'm a software developer based in Vienna, Austria. I have been working as a Java and JavaScript developer for a few major companies. For work I have been programming Java Spring Applications, as well as both Front- and Backend NodeJs Applications.
             I also love creating iOS apps using Swift and SwiftUi. But that's not all, because what I love most, is attempting new challenges and finding new problems to solve. Eagerly waiting for all the problems out there needing to be solved.
           </h2>
           </div>
           <div class="md-layout-item md-medium-size-15 md-small-size-50 md-xsmall-size-100" >
-            <img style=max-height:450px  src="../assets/octocat.png" />
+            <img style=max-height:430px  src="../assets/BearWithURL.png" />
           </div>
         </div>
       <div class="md-layout md-gutter md-alignment-center" style="padding-top: 40px;padding-bottom: 40px">
@@ -63,6 +68,10 @@
         <div class="md-layout-item md-size-85 md-small-size-65">
 
       <h3> &lt;/&gt; Back End Programming </h3>
+        </div>
+        <div class="md-layout-item md-size-85 md-small-size-65">
+
+          <h3> &lt;/&gt; Java </h3>
         </div>
 
         <div class="md-layout-item md-size-85 md-small-size-65" >
@@ -180,8 +189,11 @@ position: absolute;
   left:0;
   width: 100%;
 }
-.content{
-
+.logo{
+max-height:50px;
+  position: absolute;
+  right:0;
+  padding-right:30px
 }
 
 @keyframes blinking {
